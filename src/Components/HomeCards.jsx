e949c43b2b7c90c69f098323b59d1adb28e49892
rachael444 <nnakwuchiamaka@gmail.c0m>
2025-06-13 14:03:37 +0000

    link change

--- src/Components/HomeCards.jsx
+++ src/Components/HomeCards.jsx
@@ -9,11 +9,11 @@ export default function HomeCards({ books }) {
           <div className="rounded-sm bg-[#F87171] py-1 px-2 text-white text-sm font-semibold absolute right-0">
             <span>Feature</span>
           </div>
-          <a href="/viewbook">
+          <a href="/bookspage" className="block">
             <img src={book.imageUrl} alt={book.title} className="w-full h-96 object-cover rounded-t-md transform hover:scale-105 transition-transform duration-500 ease-in-out" />
           </a>
           <div className="bg-white p-4">
-            <a href="/viewbook">
+            <a href="/bookspage">
               <h2 className="text-xl font-semibold mb-2">{book.title}</h2>
             </a>
             <p className="text-gray-700 mb-2 truncate">{book.description}</p>
